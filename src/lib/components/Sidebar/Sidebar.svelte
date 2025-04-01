a80b01a97a97c22a1e222da6dc2ffdde7200e7d5
Peter Thomas <petethomasmusique@gmail.com>
2025-04-01 15:33:25 +0100

    upgrades: disappearing toolbar on fs

--- src/lib/components/Sidebar/Sidebar.svelte
+++ src/lib/components/Sidebar/Sidebar.svelte
@@ -2,9 +2,31 @@
     import Button from "$lib/components/Button/Button.svelte";
     import { isPlaying, showControls, toggleControls, toggleIsPlaying, toggleInfo, showInfo, showCircuit, toggleCircuit } from "$lib/stores/kaleidoscope";
     import { faDiagramProject, faSliders, faPlay, faInfo } from '@fortawesome/free-solid-svg-icons';
+    import { onMount } from "svelte";
+
+    let showSidebarInFullscreen: boolean = false;
+    function showSidebar(e: MouseEvent | TouchEvent) {
+        // @ts-ignore
+        showSidebarInFullscreen = e.clientX < 50 || $showInfo || $showCircuit || $showControls;
+    }
+    
+    onMount(() => {
+        // on mousemove, set showSidebarInFullscreen to true
+        window.addEventListener('mousemove', showSidebar);
+        // on touchstart, set showSidebarInFullscreen to true
+        window.addEventListener('touchstart', showSidebar);
+        // clean up
+        return () => {
+            window.removeEventListener('mousemove', showSidebar);
+            window.removeEventListener('touchstart', showSidebar);
+        }
+        
+    })
 </script>
 
-<aside>
+<aside 
+    class:show={showSidebarInFullscreen} 
+>
     <ul>
         <li>
             <Button 
@@ -49,6 +71,15 @@
         flex-direction: column;
         justify-content: center;
         z-index: 100;
+        transition: transform 0.5s ease-in-out;
+
+        @media all and (display-mode: fullscreen) {
+            transform: translateX(-100%);
+        }
+
+        &.show {
+            transform: translateX(0);
+        }
     }
 
     ul {
